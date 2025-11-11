dd6a68c70a442f4fc9ac2439f841eb175ac53086
Ádám Vajda <vjdad4m@gmail.com>
2025-11-11 21:00:20 +0100

    feat: implement Hint service

--- src/main/java/com/vadam/sudoku/service/HintService.java
+++ src/main/java/com/vadam/sudoku/service/HintService.java
@@ -1,4 +1,38 @@
 package com.vadam.sudoku.service;
 
+import com.vadam.sudoku.model.Board;
+import com.vadam.sudoku.solver.Solver;
+import com.vadam.sudoku.solver.Step;
+import com.vadam.sudoku.solver.strategy.NakedPairsStrategy;
+import com.vadam.sudoku.solver.strategy.SingleCandidateStrategy;
+import com.vadam.sudoku.solver.strategy.SinglePositionStrategy;
+
+import java.util.List;
+import java.util.Optional;
+
 public class HintService {
+    private final Solver solver = new Solver(
+            List.of(new SingleCandidateStrategy(), new SinglePositionStrategy(), new NakedPairsStrategy()));
+
+    public Optional<Step> nextStep(Board b) {
+        return solver.nextStep(new Board(b));
+    }
+
+    public int applyLogical(Board b) {
+        return solver.solveLogically(b).size();
+    }
+
+    public boolean solveFully(Board b) {
+        Board copy = new Board(b);
+        solver.solveLogically(b);
+        if (copy.isSolved()) {
+            b.setAll(copy.values(), copy.fixed());
+            return true;
+        }
+        boolean ok = solver.solveBacktracking(copy);
+        if (ok) {
+            b.setAll(copy.values(), copy.fixed());
+        }
+        return ok;
+    }
 }
